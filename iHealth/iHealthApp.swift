8cd305060ffa386bdbbb9a48ed5420b8893252dd
Gabe <gabe@gimme.coffee>
2026-04-07 22:21:20 +0900

    Initial Commit

---
+++ iHealth/iHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHealthApp.swift
+//  iHealth
+//
+//  Created by Gabe Perez on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct iHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
